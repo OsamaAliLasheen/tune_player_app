7e928a449307a045e7828c24087e4570c642cc65
Osama <osama.ali.lasheen@gmail.com>
2024-03-17 03:38:53 +0200

    Add TuneModel class and update TunePage to use TuneModel for tunes
    Added a new TuneModel class to represent a tune with its color. Updated the TunePage
    to use the TuneModel class for storing tunes instead of just colors. This allows for
    better organization and readability of the code.

---
+++ lib/Models/tune_model.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class TuneModel {
+  final String tune;
+  final Color color;
+
+  const TuneModel({required this.tune, required this.color});
+}
--- lib/screens/tune_screen.dart
+++ lib/screens/tune_screen.dart
@@ -1,17 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:music_notes_player_app_setup/Models/tune_model.dart';
 import 'package:music_notes_player_app_setup/components/tune_item.dart';
 
 class TunePage extends StatelessWidget {
   const TunePage({super.key});
 
-  final List<Color> tuneColors = const [
-    Color(0xffFE3F39),
-    Color(0xffFD972B),
-    Color(0xffFDEB56),
-    Color(0xff33AF57),
-    Color(0xff009587),
-    Color(0xff0097ED),
-    Color(0xffA227AC),
+  final List<TuneModel> tunes = const [
+    TuneModel(
+      tune: 'note1.wav',
+      color: Color(0xffFE3F39),
+    ),
+    TuneModel(
+      tune: 'note2.wav',
+      color: Color(0xffFD972B),
+    ),
+    TuneModel(
+      tune: 'note3.wav',
+      color: Color(0xffFDEB56),
+    ),
+    TuneModel(
+      tune: 'note4.wav',
+      color: Color(0xff33AF57),
+    ),
+    TuneModel(
+      tune: 'note5.wav',
+      color: Color(0xff009587),
+    ),
+    TuneModel(
+      tune: 'note6.wav',
+      color: Color(0xff0097ED),
+    ),
+    TuneModel(
+      tune: 'note7.wav',
+      color: Color(0xffA227AC),
+    ),
   ];
 
   @override
@@ -28,10 +50,8 @@ class TunePage extends StatelessWidget {
           ),
         ),
         body: Column(
-            children: tuneColors
-                .map((color) => TuneItem(
-                    color: color,
-                    tune: 'note${tuneColors.indexOf(color) + 1}.wav'))
+            children: tunes
+                .map((tunes) => TuneItem(color: tunes.color, tune: tunes.tune))
                 .toList()),
       ),
     );
